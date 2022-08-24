49740c169176f2f89c0bdc6c280b43c07cc2df96
Facade <mwiloveicecream565@gmail.com>
2022-08-23 23:19:20 -0600

    Basic UI component added.

---
+++ Components/ViewerUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(GraphicRaycaster))]
+public class ViewerUI : MonoBehaviour
+{
+    
+}
